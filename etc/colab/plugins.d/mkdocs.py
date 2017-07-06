b1e1a301ebdd082f256d74a15e7b12849b82a0f6
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-07-06 17:33:41 -0300

    Added new pages
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- etc/colab/plugins.d/mkdocs.py
+++ etc/colab/plugins.d/mkdocs.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-name = 'colab_mkdocs'
+name = 'colab_mkdocs_tos'
 verbose_name = 'Colab Mkdocs Plugin Plugin'
 
 upstream = ''
@@ -9,11 +9,14 @@ docs_title = "Sobre"
 
 pages = {
     'Home': 'index.md',
+    'Wikilegis': 'wikilegis.md',
+    'Audiências Interativas': 'audiencias.md',
+    'Expressão': 'expressao.md',
     'Termos de Serviço': 'tos.md'
 }
 
 
 urls = {
     'include': 'colab_mkdocs.urls',
-    'prefix': '^mkdocs/',
+    'prefix': '^sobre/',
 }
